87101bcc9f910aca0737b632050908746c9ec986
ebr <clkebr2601@gmail.com>
2024-01-10 13:42:10 +0100

    Eureka client is installed

--- product-service/src/main/java/com/micro/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/com/micro/productservice/ProductServiceApplication.java
@@ -3,11 +3,13 @@ package com.micro.productservice;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @EnableMongoRepositories
 @SpringBootApplication
+@EnableEurekaClient
 public class ProductServiceApplication {
 
     public static void main(String[] args) {
