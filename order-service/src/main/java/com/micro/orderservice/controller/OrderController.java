ba1d235f03e8ec945b2860288b5305a32a239fc5
ebr <clkebr2601@gmail.com>
2024-01-15 13:28:19 +0100

    resilience4J timeout properties implemented

--- order-service/src/main/java/com/micro/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/micro/orderservice/controller/OrderController.java
@@ -4,6 +4,7 @@ import com.micro.orderservice.dto.OrderDto;
 import com.micro.orderservice.entity.ResponseWrapper;
 import com.micro.orderservice.service.OrderService;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +12,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequestMapping("api/orders")
@@ -21,15 +24,19 @@ public class OrderController {
 
     @PostMapping
     @CircuitBreaker(name = "inventory",fallbackMethod = "inventoryFallback")
-    public ResponseEntity<ResponseWrapper> placeOrder(@RequestBody OrderDto orderDto){
+    @TimeLimiter(name = "inventory")
+    public CompletableFuture<ResponseEntity<ResponseWrapper>>  placeOrder(@RequestBody OrderDto orderDto){
 
         OrderDto createdOrder = orderService.placeOrder(orderDto);
-        return ResponseEntity.status(HttpStatus.CREATED).body(new ResponseWrapper("Order placed successfully",createdOrder, HttpStatus.CREATED));
+        return CompletableFuture.supplyAsync(()->
+                ResponseEntity.status(HttpStatus.CREATED).body(new ResponseWrapper("Order placed successfully",createdOrder, HttpStatus.CREATED)));
     }
 
-    public ResponseEntity<ResponseWrapper> inventoryFallback(OrderDto orderDto, RuntimeException runtimeException) {
-        return ResponseEntity.status(HttpStatus.OK)
-                .body(new ResponseWrapper("Oops! Something went wrong, please order again.", HttpStatus.OK));
+    public CompletableFuture<ResponseEntity<ResponseWrapper>> inventoryFallback(OrderDto orderDto, RuntimeException runtimeException) {
+        return
+                CompletableFuture.supplyAsync(()->
+                        ResponseEntity.status(HttpStatus.OK)
+                                      .body(new ResponseWrapper("Oops! Something went wrong, please order again.", HttpStatus.OK)));
     }
 
 }
