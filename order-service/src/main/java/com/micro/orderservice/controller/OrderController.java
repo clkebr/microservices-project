de35bbafb3cf6596889db0e9214a0326339084ae
ebr <clkebr2601@gmail.com>
2024-01-15 13:08:34 +0100

    resilience4J is implemented

--- order-service/src/main/java/com/micro/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/micro/orderservice/controller/OrderController.java
@@ -3,6 +3,7 @@ package com.micro.orderservice.controller;
 import com.micro.orderservice.dto.OrderDto;
 import com.micro.orderservice.entity.ResponseWrapper;
 import com.micro.orderservice.service.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,9 +20,16 @@ public class OrderController {
     private final OrderService orderService;
 
     @PostMapping
+    @CircuitBreaker(name = "inventory",fallbackMethod = "inventoryFallback")
     public ResponseEntity<ResponseWrapper> placeOrder(@RequestBody OrderDto orderDto){
 
         OrderDto createdOrder = orderService.placeOrder(orderDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(new ResponseWrapper("Order placed successfully",createdOrder, HttpStatus.CREATED));
     }
+
+    public ResponseEntity<ResponseWrapper> inventoryFallback(OrderDto orderDto, RuntimeException runtimeException) {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(new ResponseWrapper("Oops! Something went wrong, please order again.", HttpStatus.OK));
+    }
+
 }
